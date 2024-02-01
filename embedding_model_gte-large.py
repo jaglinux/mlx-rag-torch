229a84d2ead3cea6d3108e984c132ddac6c6b953
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2024-01-31 23:42:03 -0800

    Embedding_Model: Streamline the input data and query
    Signed-off-by: Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>

--- embedding_model_gte-large.py
+++ embedding_model_gte-large.py
@@ -1,21 +1,21 @@
+import torch
 import torch.nn.functional as F
 from torch import Tensor
 from transformers import AutoTokenizer, AutoModel
 
 class Embedding_Model():
-    def __init__(self, input_texts):
+    def __init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained("thenlper/gte-large")
         self.model = AutoModel.from_pretrained("thenlper/gte-large")
-        self.input_texts = input_texts
 
     def average_pool(self, last_hidden_states: Tensor,
                     attention_mask: Tensor) -> Tensor:
         last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
         return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
     
-    def create_embeddings(self):
+    def create_embeddings(self, input_texts):
         # Tokenize the input texts
-        batch_dict = self.tokenizer(self.input_texts, max_length=512, 
+        batch_dict = self.tokenizer(input_texts, max_length=512, 
                                     padding=True, truncation=True, return_tensors='pt')
 
         outputs = self.model(**batch_dict)
@@ -25,18 +25,21 @@ class Embedding_Model():
         embeddings = F.normalize(embeddings, p=2, dim=1)
         return embeddings
     
-def caclulate_scores(embeddings):
-    scores = (embeddings[:1] @ embeddings[1:].T) * 100
+def caclulate_scores_index(source_data_emb, query_data_emb):
+    scores = (query_data_emb @ source_data_emb.T) * 100
     print(scores.tolist())
-    return scores.tolist()
+    return torch.argmax(scores).item()
  
-# Code to test above embedding model   
+# Code to test above embedding model
+# query = "what is the capital of China?"
 # input_texts = [
-#     "what is the capital of China?",
 #     "how to implement quick sort in python?",
 #     "Beijing",
 #     "sorting algorithms"
 # ]
-# e = Embedding_Model(input_texts)
-# data = e.create_embeddings()
-# caclulate_scores(data)
+# e = Embedding_Model()
+# source_data_emb = e.create_embeddings(input_texts)
+# query_data_emb = e.create_embeddings(query)
+# index = caclulate_scores_index(source_data_emb, query_data_emb)
+# print(f"question is {query}")
+# print(f"index is {index} and Context is {input_texts[index]}")
